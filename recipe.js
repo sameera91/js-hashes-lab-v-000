c5591cd2bcf86b07f09af09801b61997b7332f04
sameera91 <sameera.bachoti@gmail.com>
2016-05-20 18:13:58 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function addIngredient(recipe, ingredient, amounts){
+  recipe[ingredient] = amounts;
+
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient){
+  delete recipe[ingredient];
+
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount){
+   recipe[ingredient] = amount;
+
+   return recipe;
+}
+
+function readRecipe(recipe){
+  for(var ingredient in recipe){
+    console.log("this recipe calls for " + recipe[ingredient] + " of " + ingredient);
+  }
+}
